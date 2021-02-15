305116c601864106a032e5bbfddd43aa84373ccd
Peter Knipp <peteraknipp@gmail.com>
2021-02-14 20:42:34 -0500

    Provide numerical (but parenthetical) values for ac and b.

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = ({ strings, col, colsVisible, rowsVisible }) => {
     return colsVisible < col && rowsVisible === 1 ? null :
         <th>
-            {strings[0]} <br/> {strings[1]} <br/> {strings[2]} <i>{strings[3]}</i>
+            {strings[0]} <br/> {strings[1]} <br/> {strings[2]} <i>{strings[3]}</i> {strings[4]} <br/>{strings[5]}
         </th>
 }
 export default Header;
